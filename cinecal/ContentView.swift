e7d10e803d9890e06d9458151f45b02b5b9e9b42
Vir Bedi <vir.bedi@vida.com>
2025-03-18 13:10:36 -0700

    Initial Commit

---
+++ cinecal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cinecal
+//
+//  Created by Vir Bedi on 3/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cinecal/cinecalApp.swift
@@ -0,0 +1,17 @@
+//
+//  cinecalApp.swift
+//  cinecal
+//
+//  Created by Vir Bedi on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct cinecalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
